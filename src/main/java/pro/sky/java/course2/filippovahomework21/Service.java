799dbc52bed01462656b0f312be132563ac64f6d
filippova_t <tanya-fef@yandex.ru>
2022-10-17 20:55:18 +0800

    домашнее задание 2.11 выполнено

--- src/main/java/pro/sky/java/course2/filippovahomework21/Service.java
+++ src/main/java/pro/sky/java/course2/filippovahomework21/Service.java
@@ -1,4 +1,11 @@
 package pro.sky.java.course2.filippovahomework21;
 
+import java.util.List;
+
 public interface Service {
+    void add(List<Integer> ID);
+
+
+    List<Integer> get();
 }
+
--- src/main/java/pro/sky/java/course2/filippovahomework21/ServiceImpl.java
+++ src/main/java/pro/sky/java/course2/filippovahomework21/ServiceImpl.java
@@ -1,4 +1,29 @@
 package pro.sky.java.course2.filippovahomework21;
 
-public class ServiceImpl {
+
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@org.springframework.stereotype.Service
+@SessionScope
+public class ServiceImpl implements Service {
+    private final List <Integer> basket;
+
+    public ServiceImpl() {
+        this.basket = new ArrayList<>();
+    }
+
+    @Override
+    public void add(List<Integer> IDs) {
+        basket.addAll (IDs);
+    };
+
+
+
+    @Override
+    public List<Integer> get() {
+        return basket;
+    }
 }
